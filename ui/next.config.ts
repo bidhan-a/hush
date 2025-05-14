f1193398684e4270cd35255041d598da71abb507
bidhan-a <bidhan619@gmail.com>
2025-05-14 14:29:55 +0545

    fix prisma issue

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -1,4 +1,6 @@
 import type { NextConfig } from "next";
+// @ts-expect-error: @prisma/nextjs-monorepo-workaround-plugin does not have type definition.
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -11,6 +13,12 @@ const nextConfig: NextConfig = {
   },
   typescript: {
     ignoreBuildErrors: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+    return config;
   },
 };
 
